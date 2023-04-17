62605aa37721614a99227eefa56fc6df943f67e9
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-04-17 16:16:28 +0100

    event item variables

--- components/EventItem.js
+++ components/EventItem.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 
 const EventItem = ({event}) => {
-    
+
     const name = event.name
-    const location = event._embedded.venues[0].name
+    const date = event.dates.start.localDate
+    const time = event.dates.start.localTime
+    const venue = event._embedded.venues[0].name
+    const image = event.images[1]
+
 
 
   return (
     <View>
+        <Image source={image}></Image>
         <Text>{name}</Text>
-        <Text>{location}</Text>
+        <Text>{date}</Text>
+        <Text>{time}</Text>
+        <Text>{venue}</Text>
     </View>
   )
 }
