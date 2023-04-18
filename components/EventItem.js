c421d3763f13e2c19e9f64ac89152d242d61bd27
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-04-18 14:26:19 +0100

    card styling

--- components/EventItem.js
+++ components/EventItem.js
@@ -7,7 +7,7 @@ const EventItem = ({event, increaseCounter}) => {
     const date = event.dates.start.localDate
     const time = event.dates.start.localTime
     const venue = event._embedded.venues[0].name
-    const image = event.images[4]
+    const image = event.images[1]
 
     function handleOnPress(){
         increaseCounter();
@@ -32,19 +32,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     minWidth: '90%',
+    maxWidth: '90%',
     maxHeight: '90%',
     alignItems: 'center',
     justifyContent: 'center',
     borderColor: 'black',
     borderWidth: 5,
     backgroundColor: 'gray',
-    // bottom: 50,
-    // top: 50,
+    top: 50,
+    left:18,
   },
 
   image: {
-    width: 100,
-    height: 100,
+    width: '80%',
+    height: '80%',
   },
 });
 
