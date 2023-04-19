8a035e86279d0c00aa384cc71c66790d18d1559e
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-04-19 13:17:45 +0100

    prog

--- components/EventsList.js
+++ components/EventsList.js
@@ -1,12 +1,12 @@
 import { Text, View, Button } from "react-native";
 import EventItem from "./EventItem";
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
 import TinderCard from 'react-tinder-card'
+import React from "react";
 
 const EventsList = ({events}) => {
 
-
-    let [index, setIndex] = useState(0)
+    // let [index, setIndex] = useState(0)
 
     function increaseCounter(){
         let newIndex = index += 1;
@@ -17,18 +17,96 @@ const EventsList = ({events}) => {
         return setIndex(newIndex)
     }
 
+
+
+
     let eventNodes = events.map((event, index) => {
         return <EventItem event={event} key={index} index={index}/>
     });
+
+
+
+    // let test = events.map((event, index) => {
+    //     console.log(event.name)
+    //     return [event.name]
+    // });
+    let test = events;
+
+
+    const [currentIndex, setCurrentIndex] = useState(test.length-1);
+    const [lastDirection, setLastDirection] = useState()
+    // out of frame closure???????
+    const currentIndexRef = useRef(currentIndex)
+
+    // ?????
+    const childRefs = useMemo(
+        () =>
+          Array(test.length)
+            .fill(0)
+            .map((i) => React.createRef()),
+        []
+      )
+
+      const updateCurrentIndex = (val) => {
+        setCurrentIndex(val)
+        currentIndexRef.current = val
+      }
+
+    const canGoBack = currentIndex < test.length - 1
+
+    const canSwipe = currentIndex >= 0
+
+    //  set last direction and decrease current index
+    const swiped = (direction, nameToDelete, index) => {
+    setLastDirection(direction)
+    updateCurrentIndex(index - 1)
+  }
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
+  }
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < test.length) {
+      await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
+    }
+  }
+
+  // increase current index and show card
+  const goBack = async () => {
+    if (!canGoBack) return
+    const newIndex = currentIndex + 1
+    updateCurrentIndex(newIndex)
+    await childRefs[newIndex].current.restoreCard()
+  }
 
     function handleOnPress(){
         increaseCounter();
     }
 
     return(
+
         <View>
-            {eventNodes}
-            <Button onPress={handleOnPress} title="Press me"/>
+
+            <Text>React Tinder Card</Text>
+            {/* {eventNodes} */}
+            {/* <Button onPress={handleOnPress} title="Press me"/> */}
+
+            {test.map((event, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            // className='swipe'
+            key={event.name}
+            onSwipe={(dir) => swiped(dir, event.name, index)}
+            onCardLeftScreen={() => outOfFrame(event.name, index)}
+          >
+            <Text>{event.name}</Text>
+          </TinderCard>
+        ))}
+
+        
+
         </View>
     )
 }
