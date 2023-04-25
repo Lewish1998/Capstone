3de4a1b230e5cab5d27741f149bbcbc5a1224fed
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-04-25 19:51:46 +0100

    working on account page

--- components/AccountSettings.js
+++ components/AccountSettings.js
@@ -1,7 +1,15 @@
-import React from 'react'
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React from 'react';
+import { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 const AccountSettings = () => {
+
+  const [updateEmail, setUpdateEmail] = useState("");
+  const [updatePassword, setUpdatePassword] = useState("");
+  const [updateLocation, setUpdateLocation] = useState("");
+  const[updateName, setUpdateName] = useState("");
+
+
   return (
     
     <View style={styles.container}>
@@ -9,6 +17,51 @@ const AccountSettings = () => {
         <Image source={require("../images/Oot'N'Aboot-logos_black.png")} style={{position:'absolute', width: 120, height: 80, left:110, bottom:20}}/>
       </View>
       <Text style={{fontSize:36, textAlign:'center', paddingBottom:20}}>Settings Page</Text>
+      <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Name"
+              placeholderTextColor="#003f5c"
+              onChangeText={(data) => setUpdateName(data.toLowerCase())}
+            />
+          </View>
+          
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Email"
+              placeholderTextColor="#003f5c"
+              onChangeText={(data) => setUpdateEmail(data.toLowerCase())}
+            />
+          </View>
+
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Location"
+              placeholderTextColor="#003f5c"
+              onChangeText={(data) => setUpdateLocation(data.toLowerCase())}
+            />
+          </View>
+
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Password"
+              placeholderTextColor="#003f5c"
+              secureTextEntry={true}
+              onChangeText={(data) => setUpdatePassword(data)}
+            />
+          </View>
+
+
+          <TouchableOpacity style={styles.loginBtn} >
+            <Text style={styles.loginText}> Register</Text>
+          </TouchableOpacity>
+
+
+
+
     </View>
   )
 }
@@ -22,7 +75,20 @@ const styles = StyleSheet.create({
     top: 120,
     padding:10,
     backgroundColor:'#ffffff'
-  }
+  },
+  inputView: {
+    backgroundColor: "#ffffff",
+    borderRadius: 30,
+    width: 240,
+    height: 45,
+    marginBottom: 20,
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    width: "100%",
+  },
 })
 
 export default AccountSettings;
